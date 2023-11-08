e039c45e0dedb18693021f46a0a8abf18680369f
leen-1422 <leen8288@yahoo.com>
2023-11-08 23:51:44 +0300

    requested changes

--- index.js
+++ index.js
@@ -1,99 +1,85 @@
 import http from "http";
-import fs from "fs";
+import fs from "fs/promises";
+import { parse } from "querystring";
 
 const PORT = "8080";
 
-let products = [
-  {
-    id: 1,
-    name: "Cactus",
-    price: 150,
-  },
-  {
-    id: 2,
-    name: "Flower",
-    price: 150,
-  },
-];
-
 http
-  .createServer((req, res) => {
+  .createServer(async (req, res) => {
     if (req.method === "GET" && req.url === "/") {
-      res.writeHead(200, {  "Content-Type": "text/plain" });
-      res.write("Hello, World!");
-      res.end();
-    } 
-    else if (req.method === "GET" && req.url === "/products") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.write(JSON.stringify(products));
-      res.end();
-    } 
-    else if (req.method === "POST" && req.url === "/") {
+      try {
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.write("Hello, World!");
+        res.end();
+      } catch {
+        res.write(err.message);
+        res.end();
+      }
+    } else if (req.method === "GET" && req.url === "/products") {
+      try {
+        const products = JSON.parse(
+          await fs.readFile("products.json", "utf-8")
+        );
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write(JSON.stringify(products));
+        res.end();
+      } catch (err) {
+        res.write(err.message);
+        res.end();
+      }
+    } else if (req.method === "POST" && req.url === "/") {
       let receivedData = "";
       req.on("data", (chunk) => {
         receivedData += chunk;
       });
-    
+
       req.on("end", () => {
         console.log("Received data:", receivedData);
         res.writeHead(200, { "Content-Type": "text/plain" });
         res.end("Received data: " + receivedData);
       });
-    }
-    // if(req.method==="POST" && req.url==='/products'){
+    } else if (req.method === "POST" && req.url === "/products") {
+      try {
+        let receivedData = "";
 
-    //   fs.writeFile('./products.json', JSON.stringify(products, null, 2), (error) => {
-      
-    //     if (error) {
-    //       console.log('An error has occurred ', error);
-    //       return;
-    //     }
-    //     console.log('Data written successfully to disk');
-    //   });
-    // }
+        req.on("data", (chunk) => {
+          receivedData += chunk;
+          console.log(receivedData);
+        });
+        req.on("end", async () => {
+          console.log("Received data:", parse(receivedData));
+          const valueID = parseInt(new Date().getTime());
+          const parsedData = parse(receivedData);
 
-    else if (req.method === "POST" && req.url === "/products") {
-      let receivedData = "";
-    
-      req.on("data", (chunk) => {
-        receivedData += chunk;
-      });
-    
-      req.on("end", () => {
-        try {
-          const receivedProduct = JSON.parse(receivedData);
-    
-          fs.writeFile("./products.json", JSON.stringify(receivedProduct, null, 2), (err) => {
-            if (err) {
-              console.log("Something went wrong");
-              res.statusCode = 500;
-              res.setHeader("Content-Type", "application/json");
-              res.end(JSON.stringify({ error: "Something went wrong" }));
-            } else {
-              res.statusCode = 200;
-              res.setHeader("Content-Type", "application/json");
-              res.end(JSON.stringify({ success: true }));
+          const product = {
+            id: valueID,
+            ...parsedData,
+          };
+
+          const products = JSON.parse(
+            await fs.readFile("products.json", "utf-8")
+          );
+          products.push(product);
+          await fs.writeFile(
+            "./products.json",
+            JSON.stringify(products),
+            (err) => {
+              if (err) {
+                console.error("Error writing to file:", err);
+              } else {
+                console.log("Products data written to file");
+              }
             }
-          });
-        } catch (error) {
-          console.log("Invalid JSON format");
-          res.statusCode = 400;
-          res.setHeader("Content-Type", "application/json");
-          res.end(JSON.stringify({ error: "Invalid JSON format" }));
-        }
-      });
-    }
-    // else if (req.method === "POST" && req.url === "/products") {
+          );
+        });
+        res.writeHead(201, { "Content-Type": "application/json" });
 
-    //   fs.writeFile("./products.txt", JSON.stringify(products), (err) => {
-    //     if (err) {
-    //       console.log("something went wrong");
-    //     } else {
-    //       console.log("file stored");
-    //     }
-    //   });
-    //   res.end();
-    // }
+        res.end();
+      } catch {
+        res.write(err.message);
+        res.end();
+      }
+    }
   })
   .listen(PORT, () => {
     console.log(`Server running at http://127.0.0.1:${PORT}/`);
